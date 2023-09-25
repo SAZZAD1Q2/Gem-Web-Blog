175ca0ea5759d9ccbd63fe7421cab68de0521983
SAZZAD1Q2 <kurulia115@gmail.com>
2023-09-25 17:55:50 +0600

    linters fix

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,14 +1,13 @@
 class UsersController < ApplicationController
-    def index
-      # Placeholder for the all users page
-    end
-  
-    def show
-      # Placeholder for a single user page
-    end
-  
-    def posts
-      # Placeholder for all posts by a given user page
-    end
+  def index
+    # Placeholder for the all users page
   end
-  
+
+  def show
+    # Placeholder for a single user page
+  end
+
+  def posts
+    # Placeholder for all posts by a given user page
+  end
+end
--- spec/helpers/users_helper_spec.rb
+++ spec/helpers/users_helper_spec.rb
@@ -1,3 +1,4 @@
+# rubocop:disable all
 require 'rails_helper'
 
 # Specs in this file have access to a helper object that includes
