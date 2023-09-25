88aab3da0628e2676b3cbfa2b51a1e675e157877
SAZZAD1Q2 <kurulia115@gmail.com>
2023-09-25 17:51:07 +0600

    controllers and view set

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,14 @@
+class UsersController < ApplicationController
+    def index
+      # Placeholder for the all users page
+    end
+  
+    def show
+      # Placeholder for a single user page
+    end
+  
+    def posts
+      # Placeholder for all posts by a given user page
+    end
+  end
+  
