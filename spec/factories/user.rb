307ecc2210ea271579acf84e701fde11fa412bec
SAZZAD1Q2 <kurulia115@gmail.com>
2023-09-26 21:15:54 +0600

    linters fix

--- spec/factories/user.rb
+++ spec/factories/user.rb
@@ -1,10 +1,8 @@
 # spec/factories/users.rb
 FactoryBot.define do
-    factory :user do
-      name { "John Doe" }
-      email { "john@example.com" } # Add the email attribute
-      # Add other attributes as needed
-    end
+  factory :user do
+    name { 'John Doe' }
+    email { 'john@example.com' } # Add the email attribute
+    # Add other attributes as needed
   end
-  
-  
+end
